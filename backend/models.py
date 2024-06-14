88fdbda9224c42e253cb82961fbef5fc20039ad4
Gonzalo <Gonza.23garcia@gmail.com>
2024-06-14 12:33:54 -0300

    agregue cambios en models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
@@ -7,23 +6,23 @@ db = SQLAlchemy()
 class player(db.model):
     __tablename__ = 'players'
     id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.String{31}, nullable=False, unique=True)
-    money = db.column(db.Integer)
-    created_at = dbcolumn(db.DateTime, default=datetime.datetime.now())
+    name = db.column(db.String(31), nullable=False, unique=True)
+    money = db.column(db.Integer, nullable=False)
+    created_at = db.column(db.DateTime, default=datetime.datetime.now())
 
-class building_type(db.model):
-    __tablename__ = 'building_types'
+class property_type(db.model):
+    __tablename__ = 'properties_types'
     id = db.column(db.Integer, primary_key=True)
-    category = db.column(db.String{31}, unique=True, nullable=False)
+    category = db.column(db.String(31), unique=True, nullable=False)
     time_to_build = db.column(db.DateTime, nullable=False)
-    cost = db.column(db.Integer, nullable=False)
     profits = db.column(db.Integer, nullable=False)
-    created_at = dbcolumn(db.DateTime, default=datetime.datetime.now())
+    created_at = db.column(db.DateTime, default=datetime.datetime.now())
 
-class building(db.model):
-    __tablename__ = 'buildings'
+class property(db.model):
+    __tablename__ = 'properties'
     id = db.column(db.Integer, primary_key=True)
-    id_player = db.column(db.Integer, db.ForeignKey('player.id')
-    id_building = db.column(db.Integer, db.ForeignKey('building.id')
-    built_at = dbcolumn(db.DateTime, default=datetime.datetime.now())
+    id_player = db.column(db.Integer, db.ForeignKey('player.id'))
+    id_property = db.column(db.Integer, db.ForeignKey('property.id'))
+    cost_property = db.column(db.Integer, nullable=False)
+    crated_at = db.column(db.DateTime, default=datetime.datetime.now())
 

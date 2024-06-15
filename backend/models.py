c206c8f0d4f8599456eb8fe3a5dde5a9dafbadef
Gonzalo <Gonza.23garcia@gmail.com>
2024-06-14 23:57:47 -0300

    Agregue cambios en models.py

--- backend/models.py
+++ backend/models.py
@@ -21,8 +21,9 @@ class property_type(db.Model):
 class property(db.Model):
     __tablename__ = 'properties'
     id = db.Column(db.Integer, primary_key=True)
-    id_player = db.Column(db.Integer, db.ForeignKey('player.id'))
-    id_property = db.Column(db.Integer, db.ForeignKey('property.id'))
+    id_player = db.Column(db.Integer, db.ForeignKey('players.id'))
+    id_property_type = db.Column(db.Integer, db.ForeignKey('properties_types.id'))
     cost_property = db.Column(db.Integer, nullable=False)
-    crated_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
 
